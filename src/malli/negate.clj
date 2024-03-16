f04c06ad21c549058cb885475d6a731c0fa04d0c
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-03-15 20:08:55 -0500

    wip

--- src/malli/negate.clj
+++ src/malli/negate.clj
@@ -15,14 +15,20 @@
 (defmethod -negate-schema :not [schema options] (first (m/children schema)))
 (defmethod -negate-schema :not= [schema options] (m/schema [:= (first (m/children schema))] options))
 (defmethod -negate-schema :map [schema options]
-  (-> [:or [:not #'clojure.core/map?]]
-      (into (keep (fn [[k :as e]]
-                    (when (-> e -last m/properties :optional not)
-                      [:map [k {:optional true} :never]])))
-            (m/entries schema))
-      (into (mapcat (fn [[k s :as e]]
-                      [[:map [k (negate (-> s m/children first) options)]]]))
-            (m/entries schema))))
+  (let [entries (m/entries schema)]
+    (assert (not (:closed (m/properties schema)))
+            "TODO negate :closed :map")
+    (assert (not-any? #(= ::m/default (first %))
+                      entries)
+            "TODO negate ::m/default :map")
+    (-> [:or [:not #'clojure.core/map?]]
+        (into (keep (fn [[k :as e]]
+                      (when (-> e -last m/properties :optional not)
+                        [:map [k {:optional true} :never]])))
+              (m/entries schema))
+        (into (mapcat (fn [[k s :as e]]
+                        [[:map [k (negate (-> s m/children first) options)]]]))
+              (m/entries schema)))))
 
 (defn negate
   ([?schema] (negate ?schema nil))
